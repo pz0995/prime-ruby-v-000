44f81ce40fd63783c59cf6c1d192e0fbb349bb3c
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-19 20:20:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,21 +1,19 @@
-def prime?(integer)
-   num = num.to_i
-   n = 1
-   factors = []
+def check_prime_number(num)
+ num = num.to_i
+ n = 1
+ factors = []
 
-   while (num >= n ) do
-      if  (num % n == 0 )
-          factors << n unless factors.include?(n)
-      end
-      n += 1
-   end
-   if ( factors.size > 2)
-      puts "Factors of number #{num} :-> #{factors}"
-   elsif num > 0
-      puts "#{num} is prime"
-   end
-  end
-
-  check_prime_number(num)
-
+ while (num >= n ) do
+    if  (num % n == 0 )
+        factors << n unless factors.include?(n)
+    end
+    n += 1
+ end
+ if ( factors.size > 2)
+    return false
+ elsif num > 0
+    return true
+ end
 end
+
+check_prime_number(num)
